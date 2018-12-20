7634ebe57d09f3c30d9a022773f19d4726165d67
JefersonCruz3 <45918879+JefersonCruz3@users.noreply.github.com>
2018-12-20 15:00:53 -0200

    Update and rename Módulo 03/01.js to  Módulo 03/01.js

--- Módulo 03/01.js
+++ Módulo 03/01.js
@@ -1,15 +1,19 @@
-function checaIdade(idade) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      return idade >= 18 ? resolve() : reject();
-    }, 2000);
-  });
-}
+var idade = document.querySelector('#textIdade');
+var buttonElement = document.querySelector('#buttonEnviar');
 
-checaIdade(15)
-  .then(function() {
-    console.log("Maior que 18");
-  })
-  .catch(function() {
-    console.log("Menor que 18");
-  });
+buttonElement.onclick = function (){
+	function checaIdade(idade) {
+ 		return new Promise(function(resolve, reject) {
+    		setTimeout(function() {
+      			return idade >= 18 ? resolve() : reject();
+    		}, 2000);
+  		});
+	}
+	checaIdade(idade.value)
+	  .then(function() {
+	    console.log("Maior ou igual a 18");
+	  })
+	  .catch(function() {
+	    console.log("Menor que 18");
+  	});
+}
